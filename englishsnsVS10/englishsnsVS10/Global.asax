4f339527d9605a362b66cbb8a19d30e2ab44b2e5
latioswang <latioswang@e1646d76-5836-d5b6-667c-78b6d9c12795>
2010-11-29 10:00:12 +0000

    Implemented a ugly lookup usecase under Controller = LookUp Action = Index HttpVerb = Post

---
+++ englishsnsVS10/englishsnsVS10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="englishsnsVS10.MvcApplication" Language="C#" %>
---
+++ englishsnsVS10/englishsnsVS10/Models/EnglishDictRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace englishsnsVS10.Models
+{
+    public class EnglishDictRepo
+    {   
+        private englishdictDataContext db = new englishdictDataContext();
+        public List<String> getExplanation(String queryWord) {
+           var  qresult = from ex in db.explanations
+                          from wd in db.words
+                          where ex.word_id==wd.id && wd.theWord==queryWord
+                          select ex.theExplanation;
+           return qresult.ToList();
+        }
+    }
+}
