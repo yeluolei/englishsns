29773f05db78d2f6fc5694996977c81e1c6da7de
latioswang <latioswang@e1646d76-5836-d5b6-667c-78b6d9c12795>
2010-12-05 11:33:15 +0000

    导入了一个大辞典

--- englishsnsVS10/englishsnsVS10/Controllers/LookUpController.cs
+++ englishsnsVS10/englishsnsVS10/Controllers/LookUpController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using englishsnsVS10.Models;
+using System.Text.RegularExpressions;
 namespace englishsnsVS10.Controllers
 {
     public class LookUpController : Controller
@@ -16,9 +17,20 @@ namespace englishsnsVS10.Controllers
             return View();
         }
         [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Index(string queryWord) {
+        public ActionResult Index(string queryWord)
+        {
             var exps = dictRepo.getExplanations(queryWord);
-            LookUpWordResult result = new LookUpWordResult(queryWord, exps);
+            String[] splittedexps = new String[1];
+
+            if (exps.Count() == 0)
+                splittedexps = null;
+            else
+                foreach (string s in exps)
+                {
+                    Regex rx = new Regex(@"\s+\d+\s+");
+                    splittedexps = rx.Split(s);
+                }
+            LookUpWordResult result = new LookUpWordResult(queryWord, splittedexps==null?null:splittedexps.ToList());
             return View(result);
         }
     }
