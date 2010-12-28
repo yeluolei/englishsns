1f473b232d41642aab91aefc3f13d8756b8f81c0
latioswang <latioswang@e1646d76-5836-d5b6-667c-78b6d9c12795>
2010-12-28 06:48:43 +0000

    added factory

--- englishsnsVS10/englishsnsVS10/SystemInterfaces/IEnglishDictRepo.cs
+++ englishsnsVS10/englishsnsVS10/SystemInterfaces/IEnglishDictRepo.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using englishsnsVS10.DAOimpl;
 namespace englishsnsVS10.DAO
 {
     interface IEnglishDictRepo
     {
         System.Linq.IQueryable<string> getExplanations(string queryWord);
         void Save();
+    }
+    class EnglishDictRepoFactory {
+        static EnglishDictRepo instance = null;
+        public static EnglishDictRepo getInstance() {
+            if (instance==null)
+                instance = new EnglishDictRepo();
+            return instance;
+        }
     }
 }
