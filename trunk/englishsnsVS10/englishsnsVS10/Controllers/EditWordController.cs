b48dc56ecd8284e1158e0c517cb06fca226af12a
zxykobezxy@gmail.com <zxykobezxy@gmail.com@e1646d76-5836-d5b6-667c-78b6d9c12795>
2011-01-03 07:23:38 +0000

    初步实现单词编辑，editword  view 不小心创建了两个。。。。log太大。。。不传了。。。数据库结构 改掉了

--- trunk/englishsnsVS10/englishsnsVS10/Controllers/EditWordController.cs
+++ trunk/englishsnsVS10/englishsnsVS10/Controllers/EditWordController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using englishsnsVS10.DAOimpl;
+using englishsnsVS10.datacontext;
 
 namespace englishsnsVS10.Controllers
 {
@@ -11,9 +13,38 @@ namespace englishsnsVS10.Controllers
         //
         // GET: /EditWord/
 
-        public ActionResult Index()
+        EnglishDictRepo englishRepo;
+
+        public ActionResult Index(int id)
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int id, FormCollection form)
+        {
+            string translation = form["content"];
+            explanation ex = new explanation();
+
+            explanation temp = englishRepo.GetExplanation(id);
+
+            if (temp.active == 1)
+                temp.active = 0;
+            if (temp.reference == -1)
+                ex.reference = temp.id;
+            else
+                ex.reference = temp.reference;
+
+            ex.expcontent = translation;
+            ex.createdata = DateTime.Now;
+            ex.wordname = temp.wordname;
+            ex.active = 1;
+            ex.modifier = User.Identity.Name;
+
+            englishRepo.AddExplanation(ex);
+            englishRepo.Save();
+
+            return Redirect("/lookup/index?queryword="+temp.wordname);
         }
 
     }
